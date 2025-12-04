068385cc300e9a62864f4ef3adc73714fba3e59a
**** <***@***.com>
2025-12-04 11:28:24 +0300

    Добавление вывода в консоль

--- Fake.CryptoPro/Program.cs
+++ Fake.CryptoPro/Program.cs
@@ -1,5 +1,18 @@
 ﻿// Fake.CryptoPro\Fake.CryptoPro> dotnet.exe publish .\Fake.CryptoPro.csproj -c Release -o e:\Work
+using System.Security.Cryptography;
 using System.Text;
+
+string CalculateMD5(string filename)
+{
+    using (var md5 = MD5.Create())
+    {
+        using (var stream = File.OpenRead(filename))
+        {
+            var hash = md5.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+    }
+}
 
 Console.OutputEncoding = Encoding.UTF8;
 string InFilePath = string.Empty;
@@ -24,17 +37,21 @@ if (args.Length >= 1)
 
         OutFilePath = Path.Combine(Path.GetDirectoryName(InFilePath), OutFilePath);
 
-        using (StreamWriter writer = new(OutFilePath, false))
-        {
-            writer.WriteLine($"{DateTime.Now:f}");
-            writer.WriteLine($"{string.Join("; ", args)}");
-        }
 
+        string consoleOutput = string.Empty;
         if (IsUseUserChoice)
         {
-            Console.Write("Тест ожадания пользовательского ввода, введите любой текст и нажминте Enter: ");
+            Console.WriteLine("Тест ожидания пользовательского ввода, введите любой текст нажмите Enter и закройте CLIApp");
             Console.Write("Запрос: ");
-            Console.ReadLine();
+            consoleOutput = Console.ReadLine();
+        }
+
+        using (StreamWriter writer = new(OutFilePath, false))
+        {
+            writer.WriteLine($"{"Дата:",-12}{DateTime.Now:f}");
+            writer.WriteLine($"{"Параметры:",-12}{string.Join("; ", args)}");
+            writer.WriteLine($"{"Консоль:",-12}{consoleOutput}");
+            writer.WriteLine($"{"MD5:",-12}{CalculateMD5(InFilePath)}");
         }
 
         Console.WriteLine($"Create file: {OutFilePath}{Environment.NewLine}Success!!!");
@@ -44,16 +61,16 @@ if (args.Length >= 1)
 {
     Console.WriteLine("Не верные параметры, ожидается:");
     Console.WriteLine();
-    Console.WriteLine($"{"c:\\Work\\FileName.txt",-20} : путь к обрабатываемогу файлу");
+    Console.WriteLine($"{"c:\\Work\\FileName.txt",-20} : путь к обрабатываемому файлу");
     Console.WriteLine($"{"signature.sig",-20} : имя создаваемого файла, в том же каталоге (опционально)");
     Console.WriteLine($"{"-k",-20} : признак запроса команды (опционально)");
     Console.WriteLine();
     Console.WriteLine($"Примечание:");
-    Console.WriteLine($"\tисползовать только полные пути в кавычках");
-    Console.WriteLine($"\tисползовать только полные пути в кавычках");
-    Console.WriteLine($"\tизбегать кирилических имён");
+    Console.WriteLine($"\tиспользовать только полные пути в кавычках");
+    Console.WriteLine($"\tиспользовать только полные пути в кавычках");
+    Console.WriteLine($"\tизбегать кириллических имён");
     Console.WriteLine($"\tизбегать каталогов с пробелами");
-    Console.WriteLine($"\tне использовать системные каталоги и корени диска");
+    Console.WriteLine($"\tне использовать системные каталоги и корни диска");
     Console.WriteLine();
     Console.WriteLine($"Пример:");
     Console.WriteLine();
